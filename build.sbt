b3b12e9b0fc24d8d8cdfaf55d01904b0bffdf89c
unknown <subhash_padala@yahoo.co.uk>
2015-09-28 13:50:10 +0100

    first commit

---
+++ build.sbt
@@ -0,0 +1,24 @@
+name := "GraphStore"
+
+version := "1.0"
+
+scalaVersion := "2.10.6"
+
+val Oversion = "2.1.2"
+val titanV = "0.9.0-M2"
+
+fork := true
+
+libraryDependencies ++= Seq(
+  "com.michaelpollmeier" % "gremlin-scala_2.10" % "3.0.0.M9-incubating",
+  "com.michaelpollmeier" % "orientdb-gremlin" % "3.0.0.M1a",
+  "org.scalatest" %% "scalatest" % "2.2.5" % Test,
+  "com.orientechnologies" % "orientdb-client" % Oversion,
+  //  "com.orientechnologies" % "orientdb-enterprise" % Oversion,
+  /*"com.orientechnologies" % "orientdb-core" % Oversion,*/
+
+  "com.thinkaurelius.titan" % "titan-cassandra" % titanV,
+  "com.thinkaurelius.titan" % "titan-core" % titanV
+
+  //"org.slf4j" % "slf4j-simple" % "1.6.4"
+)
---
+++ src/main/scala/com/evaluate/orient/DataLoader.scala
@@ -0,0 +1,23 @@
+package com.evaluate.orient
+
+import gremlin.scala.ScalaGraph
+import org.apache.tinkerpop.gremlin.orientdb.OrientGraphFactory
+
+/**
+ * Created by subhash on 28/09/2015.
+ */
+object DataLoader {
+  def main(args: Array[String]): Unit = {
+    val graph = new OrientGraphFactory("remote:localhost/barcap","admin","admin").getNoTx
+    val sg = ScalaGraph(graph)
+
+    val marko = sg.addVertex("person", Map("name" -> "marko", "age" -> 29))
+    val vadas = sg.addVertex("person", Map("name" -> "vadas", "age" -> 27))
+    val lop = sg.addVertex("software", Map("name" -> "lop", "lang" -> "java"))
+    val josh = sg.addVertex("person", Map("name" -> "josh", "age" -> 32))
+    val ripple = sg.addVertex("software", Map("name" -> "ripple", "lang" -> "java"))
+    val peter = sg.addVertex("person", Map("name" -> "peter", "age" -> 35))
+
+  }
+
+}
